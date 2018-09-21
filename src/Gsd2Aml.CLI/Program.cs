51d220dcc5aefa15aa7eef63b083632fb20284a7
Michael Weidmann <michaelweidmann@web.de>
2018-09-21 15:42:03 +0200

    added functionality to parse through the cli parameters

--- src/Gsd2Aml.CLI/Program.cs
+++ src/Gsd2Aml.CLI/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace Gsd2Aml.CLI
+﻿using System.Collections.Generic;
+
+namespace Gsd2Aml.CLI
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Dictionary<string, string> parameter = new Dictionary<string, string>
+            {
+                { "-path", null },
+                { "-file", null }
+            };
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (parameter.ContainsKey(args[i]))
+                {
+                    parameter[args[i]] = args[++i];
+                }
+            }
         }
     }
 }
