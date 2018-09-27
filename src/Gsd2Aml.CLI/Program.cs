90e4d42188059734ae484afb3d85f9dacbed7960
Michael Weidmann <michaelweidmann@web.de>
2018-09-27 17:44:40 +0200

    Added full funcionality to parse the CLI Arguments

--- src/Gsd2Aml.CLI/Program.cs
+++ src/Gsd2Aml.CLI/Program.cs
@@ -1,24 +1,79 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Gsd2Aml.CLI
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private const string CPathToFile = "--path";
+        private const string CPathToFileShort = "-p";
+
+        private const string CStringOutput = "--string";
+        private const string CStringOutputShort = "-s";
+
+        private const string CHelp = "--help";
+        private const string CHelpShort = "-h";
+        private static readonly string HelpText = $"{Environment.NewLine}GSD2AML Converter{Environment.NewLine}{Environment.NewLine}Converts a GSD-formatted file in a AML-formatted file.{Environment.NewLine}{Environment.NewLine}Usage:{Environment.NewLine}\tgsd2aml [-p | --path] <path-to-gsd-file> [options]{Environment.NewLine}{Environment.NewLine}Options:{Environment.NewLine}\t-p, --path file\tThe path to the file which should be converted. (REQUIRED){Environment.NewLine}\t-s, --string\tSet the output type to a string. Default: the output type is a file. (OPTIONAL){Environment.NewLine}";
+
+        private static void Main(string[] args)
         {
-            Dictionary<string, string> parameter = new Dictionary<string, string>
+            var parameter = new Dictionary<string, string>
             {
-                { "-path", null },
-                { "-file", null }
+                { CPathToFile, null },
+                { CPathToFileShort, null },
+                { CStringOutput, null },
+                { CStringOutputShort, null}
             };
 
-            for (int i = 0; i < args.Length; i++)
+            if (args.Length == 0)
             {
-                if (parameter.ContainsKey(args[i]))
+                PrintHelpText();
+            }
+
+            ParseCliParameter(args, parameter);
+
+            if (File.Exists(args[0]))
+            {
+                // TODO: call converter
+            }
+            else if (File.Exists(parameter[CPathToFileShort]))
+            {
+                // TODO: call converter
+            }
+            else if (File.Exists(parameter[CPathToFile]))
+            {
+                // TODO: call converter
+            }
+            else
+            {
+                Console.WriteLine($"{Environment.NewLine}File not found. Please enter a valid path to a GSD-formatted file.{Environment.NewLine}For more information: 'gsd2aml --help'.");
+                Environment.Exit(1);
+            }
+        }
+
+        private static void ParseCliParameter(IReadOnlyList<string> args, IDictionary<string, string> parameter)
+        {
+            for (var i = 0; i < args.Count; i++)
+            {
+                if (args[i].Equals(CHelp) || args[i].Equals(CHelpShort))
                 {
-                    parameter[args[i]] = args[++i];
+                    PrintHelpText();
+                }
+                else if (i + 1 < args.Count)
+                {
+                    if (parameter.ContainsKey(args[i]))
+                    {
+                        parameter[args[i]] = args[i + 1];
+                    }
                 }
             }
+        }
+
+        private static void PrintHelpText()
+        {
+            Console.WriteLine(HelpText);
+            Environment.Exit(0);
         }
     }
 }
