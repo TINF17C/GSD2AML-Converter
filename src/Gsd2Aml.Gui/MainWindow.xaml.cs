1770cd345bb9eec4efa25393e06218b4fe16654a
Steffen Gerdes <steffen@mail-gerdes.de>
2019-03-07 17:47:17 +0100

    FileDialogs
    - added FileDialogs when pressing browse buttons

--- src/Gsd2Aml.Gui/MainWindow.xaml.cs
+++ src/Gsd2Aml.Gui/MainWindow.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
-namespace Gda2Aml.Gui
+namespace Gsd2Aml.Gui
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
@@ -23,6 +12,47 @@ namespace Gda2Aml.Gui
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BrowseGsdFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                CheckFileExists = true,
+                DefaultExt = ".gsd",
+                Filter = "Generic station description files (.gsdml; .xml)|*.gsdml;*.xml",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                Title = "GSD2AML Converter"
+            };
+
+            if (dialog.ShowDialog(this).Value)
+            {
+                TxtGsdFile.Text = dialog.FileName;
+            }
+        }
+
+        private void BrowseAmlFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new SaveFileDialog
+            {
+                AddExtension = true,
+                CheckPathExists = true,
+                DefaultExt = ".amlx",
+                Filter = "AutomationML archives (.amlx)|*.amlx",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                Title = "GSD2AML Converter",
+                ValidateNames = true
+            };
+
+            if (dialog.ShowDialog(this).Value)
+            {
+                TxtAmlFile.Text = dialog.FileName;
+            }
+        }
+
+        private void Convert_OnClick(object sender, RoutedEventArgs e)
+        {
+            //ToDo: Do conversion
         }
     }
 }
