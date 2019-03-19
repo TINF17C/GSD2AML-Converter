a4569c3573d3804d8c7e67af0d18b063e8e83f97
Constantin Ruhdorfer <constantin.ruhdorfer@gmx.de>
2019-03-19 16:55:44 +0100

    Added logger to test project

---
+++ src/Gsd2Aml.Test/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gsd2Aml.Lib.Logging;
+
+namespace Gsd2Aml.Test
+{
+    class Logger : Lib.Logging.ILoggingService
+    {
+        public void Log(LogLevel level, string message)
+        {
+            Console.WriteLine(level.ToString() + message);
+        }
+    }
+}
