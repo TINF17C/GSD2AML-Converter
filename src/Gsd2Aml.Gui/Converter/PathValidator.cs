98bc195e00016f1afac3b55c5ada84a7fd53a28b
Steffen Gerdes <steffen@mail-gerdes.de>
2019-05-03 11:33:01 +0200

    added path and file validation in Gui

---
+++ src/Gsd2Aml.Gui/Converter/PathValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+
+namespace Gsd2Aml.Gui.Converter
+{
+    class PathValidator : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length != 2)
+                return false;
+            
+            if (values[0] is string gsdFile && !string.IsNullOrWhiteSpace(gsdFile) &&
+                values[1] is string amlFile && !string.IsNullOrWhiteSpace(amlFile))
+            {
+                return File.Exists(gsdFile) && Path.IsPathRooted(amlFile) &&
+                    Path.GetExtension(amlFile).Equals(".amlx", StringComparison.CurrentCultureIgnoreCase);
+            }
+
+            return false;
+        }
+        
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
