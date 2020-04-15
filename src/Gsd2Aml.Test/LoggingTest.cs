86e5a8a664a8fe9e55ea0e232df86187c004e184
Steffen Gerdes <steffen@mail-gerdes.de>
2020-04-15 23:52:41 +0200

    Updated Copyright

--- src/Gsd2Aml.Test/LoggingTest.cs
+++ src/Gsd2Aml.Test/LoggingTest.cs
@@ -1,4 +1,21 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿/*
+ *  Copyright (C) 2019 GSD2AML Team (Nico Dietz, Steffen Gerdes, Constantin Ruhdorfer,
+ *  Jonas Komarek, Phuc Quang Vu, Michael Weidmann)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation version 3 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NLog;
 using NLog.Targets;
 using NLog.Targets.Wrappers;
